6537062160e2b45c3e64e0c1314de9f52720d6b7
Pavi-gr <pavir112001@gmail.com>
2025-06-06 12:24:49 +0530

    Selenium fisrt commit

---
+++ src/org/day1/Login.java
@@ -0,0 +1,25 @@
+package org.day1;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.apache.commons.io.FileUtils;
+
+public class Login {
+	private void id() throws ClassNotFoundException,IOException {
+		Class.forName("sfvnkfvn");
+		System.out.println("11");
+		throw new  ArithmeticException("hi");
+	}
+public static void main(String[] args)  {
+	
+		Login l=new Login();
+		l.id();
+	}
+
+}
